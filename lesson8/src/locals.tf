adcc442d7350f2ce7e85c4c94e2915137ba915c7
ArutyunGLS <arutyungls@oxygen>
2023-08-05 21:29:50 +0300

    Cleaned up all unnecessary

--- lesson8/src/locals.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  web_platform_name = "${var.vm_yandex_compute_instance_global_name}-${var.vm_web_yandex_compute_instance_platform_name}"
-  db_platform_name = "${var.vm_yandex_compute_instance_global_name}-${var.vm_db_yandex_compute_instance_platform_name}"  
-  vm_metadata_override = {ssh-keys = "ubuntu:${file("~/.ssh/id_ed25519.pub")}"}
-}
