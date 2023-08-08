a255bb34cc8f7be54763b67271dc07b7cbb5a139
ArutyunGLS <arutyungls@oxygen>
2023-08-08 10:19:31 +0300

    Add upload state in bucket

--- src/providers.tf
+++ src/providers.tf
@@ -5,6 +5,17 @@ terraform {
     }
   }
   required_version = ">=0.13"
+
+  backend "s3" {
+  endpoint = "storage.yandexcloud.net"
+  bucket = "tfstate-5"
+  region = "ru-central1"
+  key = "terraform.tfstate"
+  skip_region_validation = true
+  skip_credentials_validation = true
+  dynamodb_endpoint = "https://docapi.serverless.yandexcloud.net/ru-central1/b1gu25rsl7bcn0ehonom/etn3i9g9elhg5kd71meq"
+  dynamodb_table = "tfstate-5-table"
+}
 }
 
 provider "yandex" {
